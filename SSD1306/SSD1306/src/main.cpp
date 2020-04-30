d176a0c8b483bf0eeb6c8d36ba2be3bba505aeef
MCLEANS <jackagwaya@gmail.com>
2020-04-30 20:20:43 +0300

    SSD1306 HELLO WORLD

--- SSD1306/SSD1306/src/main.cpp
+++ SSD1306/SSD1306/src/main.cpp
@@ -4,8 +4,16 @@
 #include <SSD1306Wire.h>
 
 #define LED_PIN 16
+#define SDA_PIN D1
+#define SCL_PIN D2
+
+SSD1306Wire display(0x3C,SDA_PIN,SCL_PIN);
 
 void setup(){
+    display.init();
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(10,10,"HELLO WORLD");
+    display.display();
 
     pinMode(LED_PIN,OUTPUT);
     digitalWrite(LED_PIN,LOW);
