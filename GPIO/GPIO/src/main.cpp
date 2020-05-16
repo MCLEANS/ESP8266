aeeacf9ed884f21cfc51ac9e86a1d0caea6ea22b
MCLEANS <jackagwaya@gmail.com>
2020-05-16 22:02:29 +0300

    display setup

--- GPIO/GPIO/src/main.cpp
+++ GPIO/GPIO/src/main.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "SSD1306Wire.h"
+
+SSD1306Wire display(0x3C,D1,D2); //oled i2c address and pins
+
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_16);
+
+  display.drawString(10,10,"HELLO WORLD");
 }
 
 void loop() {
