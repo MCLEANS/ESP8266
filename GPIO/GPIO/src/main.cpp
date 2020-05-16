8f86ef23d671ae190914df0b0bf3e227a5a2fc58
MCLEANS <jackagwaya@gmail.com>
2020-05-16 22:19:16 +0300

    Built in led blinky

--- GPIO/GPIO/src/main.cpp
+++ GPIO/GPIO/src/main.cpp
@@ -2,20 +2,34 @@
 #include <Wire.h>
 #include "SSD1306Wire.h"
 
-SSD1306Wire display(0x3C,D1,D2); //oled i2c address and pins
+#define OLED_SDA D1
+#define OLED_SCL D2
+#define INBUILT_LED 1
+
+SSD1306Wire display(0x3C,OLED_SDA,OLED_SCL); //oled i2c address and pins
 
 
 
 
 void setup() {
   Serial.begin(9600);
+  //----setup pin functions------------
+  pinMode(INBUILT_LED,OUTPUT);
+
   display.init();
   display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_16);
+  display.setFont(ArialMT_Plain_10);
 
-  display.drawString(10,10,"HELLO WORLD");
+  display.drawString(30,30,"DONE SETUP");
+  display.display();
+  delay(2000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(INBUILT_LED,LOW);
+  delay(1000);
+  digitalWrite(INBUILT_LED,HIGH);
+  delay(1000);
+
+  
 }
