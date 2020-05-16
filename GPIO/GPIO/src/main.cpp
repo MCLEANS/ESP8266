ff9840f6d8748e80b41203a0d8a6c36286f9dafa
MCLEANS <jackagwaya@gmail.com>
2020-05-16 22:44:06 +0300

    incremented delay

--- GPIO/GPIO/src/main.cpp
+++ GPIO/GPIO/src/main.cpp
@@ -5,6 +5,10 @@
 #define OLED_SDA D1
 #define OLED_SCL D2
 #define INBUILT_LED 16
+uint16_t duration = 400;
+
+bool increase = true;
+bool decrease = false;
 
 SSD1306Wire display(0x3C,OLED_SDA,OLED_SCL); //oled i2c address and pins
 
@@ -20,22 +24,38 @@ void setup() {
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_16);
 
-  display.drawString(30,30,"DONE SETUP");
+  display.drawString(0,30,"DONE SETUP");
   display.display();
   delay(2000);
   display.clear();
 }
 
 void loop() {
+
+  if(increase) duration+=100;
+  if(decrease) duration-=100;
+
+  if(duration == 5000) {
+    decrease = true;
+    increase = false;
+  }
+  if(duration == 300){
+    decrease = false;
+    increase = true;
+  }
+
+  
   digitalWrite(INBUILT_LED,LOW);
-  display.drawString(50,50,"LED ON");
+  display.drawString(20,20,"LED ON");
+  display.drawString(10,40,String(duration));
   display.display();
-  delay(1000);
+  delay(duration);
   display.clear();
   digitalWrite(INBUILT_LED,HIGH);
-  display.drawString(50,50,"LED ON");
+  display.drawString(20,20,"LED OUT");
+  display.drawString(10,40,String(duration));
   display.display();
-  delay(1000);
+  delay(duration);
   display.clear();
 
   
