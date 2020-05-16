b319f4b783ee6bb1bd1243723391a60bd9a5afd7
MCLEANS <jackagwaya@gmail.com>
2020-05-16 22:25:52 +0300

    print led status

--- GPIO/GPIO/src/main.cpp
+++ GPIO/GPIO/src/main.cpp
@@ -4,7 +4,7 @@
 
 #define OLED_SDA D1
 #define OLED_SCL D2
-#define INBUILT_LED 1
+#define INBUILT_LED 16
 
 SSD1306Wire display(0x3C,OLED_SDA,OLED_SCL); //oled i2c address and pins
 
@@ -18,18 +18,25 @@ void setup() {
 
   display.init();
   display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_10);
+  display.setFont(ArialMT_Plain_16);
 
   display.drawString(30,30,"DONE SETUP");
   display.display();
   delay(2000);
+  display.clear();
 }
 
 void loop() {
   digitalWrite(INBUILT_LED,LOW);
+  display.drawString(50,50,"LED ON");
+  display.display();
   delay(1000);
+  display.clear();
   digitalWrite(INBUILT_LED,HIGH);
+  display.drawString(50,50,"LED ON");
+  display.display();
   delay(1000);
+  display.clear();
 
   
 }
